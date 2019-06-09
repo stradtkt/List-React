39d6f38b27dec0cdfb4beb5cc05e393044077151
Kevin <stradtkt22@gmail.com>
2019-06-08 20:36:33 -0400

    Added the get all items route

---
+++ routes/api/items.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const Item = require('./../../models/Item');
+
+router.get('/', (req, res) => {
+    Item.find({})
+        .sort({date: -1})
+        .then(items => res.json(items))
+        .catch(err => console.log('There was an error', err));
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
 const db = require('./config/keys').mongoURI;
+
 app.use(express.json());
+app.use('./api/items', require('./routes/api/items'));
+
 mongoose.connect(db)
     .then(() => console.log('MongoDB Connected'))
     .catch(err => console.log('There was an error', err));
 
+app.get('/', (req, res) => {
+    res.send('App HERE');
+});
 
 app.listen(port, () => console.log(`App is running on port ${port}`));
 
