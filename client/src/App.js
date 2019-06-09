3a49858e7f8d48109fee2efcb7e91454411faf70
Kevin <stradtkt22@gmail.com>
2019-06-09 01:28:58 -0400

    Added a navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppNavbar from './components/AppNavbar';
+
 
 const App = () => {
   return (
     <div className="App">
-
+        <AppNavbar />
     </div>
   );
 }
