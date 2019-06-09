b6848d17ba4eca3332910ffd9cbe9bf5afab38f1
Kevin <stradtkt22@gmail.com>
2019-06-09 07:38:06 -0400

    Added shoppinglist

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 import AppNavbar from './components/AppNavbar';
+import ShoppingList from './components/ShoppingList';
 
 
 const App = () => {
   return (
     <div className="App">
         <AppNavbar />
+        <ShoppingList/>
     </div>
   );
 }
