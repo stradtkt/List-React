c35c4e4b7c0981a07f4bf9234900441e19d41b4a
Kevin <stradtkt22@gmail.com>
2019-06-08 20:25:54 -0400

    Added the item model

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb://kevin:kevin123@ds235197.mlab.com:35197/mern-kts"
+};
--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5000;
+const db = require('./config/keys').mongoURI;
 app.use(express.json());
+mongoose.connect(db)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log('There was an error', err));
+
+
 app.listen(port, () => console.log(`App is running on port ${port}`));
 
